75247fb48fd4d4f6324708eb9542f65e5391b2e8
Lucas1188 <tan.lucas1188@gmail.com>
2024-07-05 00:39:17 +0800

    Update packages

---
+++ routes/transact.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const verifyToken = require('../middleware/authMiddleware');
+const router = express.Router();
+
+router.get('/obtain_records/pending/:last', verifyToken, (req, res) => {
+  //Do something with the DB to obtain
+  res.json({ message: 'This is a sample message' });
+});
+
+router.get('/obtain_records/complete/:last',verifyToken,(req, res) => {
+  res.json({ message: 'This is a sample message' });
+});
+  
+
+
+module.exports = router;
