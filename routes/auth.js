951289015086506777d3b24116e9b67adf7ff256
Lucas1188 <tan.lucas1188@gmail.com>
2024-07-05 23:12:27 +0800

    Added User model place holder and app code parameter

--- routes/auth.js
+++ routes/auth.js
@@ -5,33 +5,39 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+var user = {};
+//Place Holder User model function
+async function  makeUser(username,password){
+    user =  {username:username,password:await bcrypt.hash(password, 10)}
+}
 // User registration
 router.post('/register', async (req, res) => {
     try {
         const { username, password } = req.body;
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = { username, password: hashedPassword };
+        //const hashedPassword = await bcrypt.hash(password, 10);
+        await makeUser(username,password)
         //await user.save();
-            res.status(201).json({ message: 'User registered successfully: ${user}'});
-        }
-        catch (error) {
-            res.status(500).json({ error: 'Registration failed' });
+        res.status(201).json({ message: `User registered successfully: ${user.username}`});
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).json({ error: 'Registration failed' });
     }
 });
 
 // User login:
 router.post('/login', async (req, res) => {
     try {
-        const { username, password } = req.body;
-        const user = await User.findOne({ username });
-        if (!user) {
+        const { username, password, app_code } = req.body;
+        //const user = {username:username, password:"123456"}
+        if (!user || username!=user.username) {
             return res.status(401).json({ error: 'Authentication failed' });
         }
         const passwordMatch = await bcrypt.compare(password, user.password);
         if (!passwordMatch) {
             return res.status(401).json({ error: 'Authentication failed' });
         }
-        const token = jwt.sign({ userId: user._id }, 'your-secret-key', {
+        const token = jwt.sign({ userId: user._id, app_code:app_code }, 'your-secret-key', {
             expiresIn: '1h',
         });
         res.status(200).json({ token });
