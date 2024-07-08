2918c54907aa14949c26e6dfeb7dbacac8b45aa7
Lucas1188 <tan.lucas1188@gmail.com>
2024-07-08 22:39:52 +0800

    return decoded values to calling route

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -16,6 +16,7 @@ function verifyToken(req, res, next) {
     if (!token) return res.status(401).json({ error: 'Access denied' });
     try {
         const decoded = jwt.verify(token, 'your-secret-key');
+        req.body.token = decoded;
         return next();
     } 
     catch (error) {
@@ -23,6 +24,7 @@ function verifyToken(req, res, next) {
         res.status(401).json({ error: 'Invalid token' });
     }
 };
+
 function verifyB2BToken(req, res, next) {
     /*
    #swagger.autoHeaders = false 
