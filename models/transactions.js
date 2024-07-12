48c6b70e9872e05e819528b97b355ce738581c01
Lucas1188 <64507210+Lucas1188@users.noreply.github.com>
2024-07-13 02:11:34 +0800

    Updated to split into first name and last name

--- models/transactions.js
+++ models/transactions.js
@@ -9,7 +9,8 @@ class TransactionRecord {
         this.loyalty_pid = sqlrow.loyalty_pid;
         this.user_id = sqlrow.user_id;
         this.member_id = sqlrow.member_id;
-        this.member_name = sqlrow.member_name;
+        this.member_first = sqlrow.member_first;
+        this.member_last = sqlrow.member_last;
         this.transaction_date = sqlrow.transaction_date;
         this.reference_number = sqlrow.reference_number;
         this.amount = sqlrow.amount;
@@ -25,7 +26,8 @@ class TransactionRecord {
             loyalty_pid TEXT NOT NULL,
             user_id TEXT NOT NULL,
             member_id TEXT NOT NULL,
-            member_name TEXT NOT NULL,
+            member_first TEXT NOT NULL,
+            member_last TEXT NOT NULL,
             transaction_date TEXT NOT NULL,
             reference_number TEXT NOT NULL UNIQUE,
             amount REAL NOT NULL,
@@ -48,7 +50,8 @@ class TransactionRecord {
                 loyalty_pid = ?,
                 user_id = ?,
                 member_id = ?,
-                member_name = ?,
+                member_first = ?,
+                member_last = ?,
                 transaction_date = ?,
                 amount = ?,
                 status = ?,
@@ -58,12 +61,11 @@ class TransactionRecord {
     }
 
     insertSQL() {
-        return `INSERT INTO ${tblname} (t_id, app_id, loyalty_pid, user_id, member_id, member_name, transaction_date, reference_number, amount, status, additional_info)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`
+        return `INSERT INTO ${tblname} (t_id, app_id, loyalty_pid, user_id, member_id, member_first,member_last, transaction_date, reference_number, amount, status, additional_info)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`
     }
     getAccrualRow(idx){
-        var name = this.member_name.split(" ");//need decrypt/encrypt
-        return [idx,this.member_id,name[0],name[1],this.transaction_date,this.amount,this.reference_number,this.app_id];
+        return [idx,this.member_id,this.member_first,this.member_last,this.transaction_date,this.amount,this.reference_number,this.app_id];
     }
     static getPendingRecords(last, callback) {
         const sql = `SELECT * FROM ${tblname} WHERE transaction_date > ? AND status = 'pending' ORDER BY transaction_date ASC`;
@@ -126,7 +128,8 @@ class TransactionRecord {
             transaction.loyalty_pid,
             transaction.user_id,
             transaction.member_id,
-            transaction.member_name,
+            transaction.member_first,
+            transaction.member_last,
             transaction.transaction_date,
             transaction.reference_number,
             transaction.amount,
@@ -172,7 +175,8 @@ async function update_transaction_record(dto, success, fail) {
                     dto.loyalty_pid,
                     dto.user_id,
                     dto.member_id,
-                    dto.member_name,
+                    dto.member_first,
+                    dto.member_last,
                     dto.transaction_date,
                     dto.amount,
                     dto.status,
