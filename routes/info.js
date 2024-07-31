162f146e18d61d92385e02e027442bcd4037b49c
Lucas1188 <64507210+Lucas1188@users.noreply.github.com>
2024-08-01 00:00:28 +0800

    minor fix for async handling

--- routes/info.js
+++ routes/info.js
@@ -6,14 +6,14 @@ const BankAppInfo = require('../models/bank.js');
 
 
 //#feature 1
-router.get('/get-loyalty-programs',AuthMiddleware.verifyToken,(req,res)=>{
+router.get('/get-loyalty-programs',AuthMiddleware.verifyToken,async (req,res)=>{
     try{
     const { token } = req.body;
-    BankAppInfo.getBankAppInfo(token['app'],(err,bai)=>{
+    await BankAppInfo.getBankAppInfo(token['app'],async (err,bai)=>{
         if(!err){
-            console.log("Getting Banking App LP list");
+            //console.log("Getting Banking App LP list");
             var lp_arr = bai.getLPArray();
-            LoyaltyPrograms.get_loyalty_program(lp_arr,(err,rows)=>{
+            await LoyaltyPrograms.get_loyalty_program(lp_arr,(err,rows)=>{
                 if(err){
                     console.error(err);
                     return res.status(500).json('error');
