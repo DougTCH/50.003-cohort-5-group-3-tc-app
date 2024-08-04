73fd139c15d71b3f25b97634d5d6e5c6241a5096
Lucas1188 <64507210+Lucas1188@users.noreply.github.com>
2024-08-04 23:44:49 +0800

    delete downloaded handbackfiles

--- jobs/gethandback.js
+++ jobs/gethandback.js
@@ -61,7 +61,9 @@ async function get_handback_job(){
           await SFTPService.Client.connect(SFTPService.Client.params);
           const fileList = await SFTPService.Client.client.list(SFTPService.Client.params.hb_path);
           for(let fn of fileList){
-               await SFTPService.Client.client.fastGet(join(SFTPService.Client.params.hb_path,fn.name),`./HandBackFiles/${fn.name}`);
+               let remotefn = join(SFTPService.Client.params.hb_path,fn.name);
+               await SFTPService.Client.client.fastGet(remotefn,`./HandBackFiles/${fn.name}`);
+               await SFTPService.Client.client.delete(remotefn);
                toprocess.push(`./HandBackFiles/${fn.name}`);
           }
      }catch(err){
